716c2262e78358656c802d60a23a980ee0a89f0b
Teofana-Ionela Moisi <teofana.moisi@gmail.com>
2020-01-11 16:17:27 +0200

    MPI done

---
+++ ProjectMPI/cmake-build-debug/CMakeFiles/ProjectMPI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectMPI.dir/main.cpp.o"
+  "ProjectMPI"
+  "ProjectMPI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectMPI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ProjectMPI/cmake-build-debug/CMakeFiles/ProjectMPI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProjectMPI.
+# This may be replaced when dependencies are built.
